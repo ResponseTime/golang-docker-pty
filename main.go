7bd26963d8e80004aa5d268eefccd738e0f662fd
responsetime <aayush.bhattacharjee2002@gmail.com>
2025-07-03 15:18:21 +0530

    added timeout based session creation of terminal need to add docker sdk to make containers

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"net"
 	"os/exec"
+	"strconv"
+	"strings"
+	"time"
 
 	"github.com/creack/pty"
 )
@@ -22,22 +26,31 @@ func main() {
 		}
 
 		go func(c net.Conn) {
-			// for {
-			// 	buffer := make([]byte, 1024)
-			// 	n, err := c.Read(buffer)
-			// 	if err != nil {
-			// 		fmt.Println("Read error:", err)
-			// 		c.Close()
-			// 		return
-			// 	}
-			// 	fmt.Printf("Received: %s\n", string(buffer[:n]))
-			// 	c.Write([]byte("hello from tcp server\n"))
-			// }
+			buffer := make([]byte, 32)
+			c.Write([]byte("Enter the amount of time you need the container for in minutes\n"))
+			n, err := c.Read(buffer)
+			if err != nil {
+				fmt.Println("Read error:", err)
+				c.Close()
+				return
+			}
+			timeReq, err := strconv.Atoi(strings.TrimSpace(string(buffer[:n])))
+			if err != nil {
+				c.Write([]byte("Time cannot be parsed"))
+			}
+			ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeReq)*time.Minute)
 			cmd := exec.Command("bash")
 			ptmx, err := pty.Start(cmd)
 			if err != nil {
 				fmt.Println(err)
 			}
+			go func() {
+				<-ctx.Done()
+				cancel()
+				c.Write([]byte("Session ended goodbye"))
+				cmd.Process.Kill()
+				c.Close()
+			}()
 			go io.Copy(c, ptmx)
 			go io.Copy(ptmx, c)
 		}(conn)
